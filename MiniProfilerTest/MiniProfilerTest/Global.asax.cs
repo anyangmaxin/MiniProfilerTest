fd05c16ebd979a84905912cbd5550cefcacc5bcd
马鑫 <anyangmaxin@163.com>
2016-08-05 15:49:16 +0800

    Global.asax文件中添加监控的启动与停止代码

--- MiniProfilerTest/MiniProfilerTest/Global.asax.cs
+++ MiniProfilerTest/MiniProfilerTest/Global.asax.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using StackExchange.Profiling;
 
 namespace MiniProfilerTest
 {
@@ -16,6 +17,19 @@ namespace MiniProfilerTest
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        protected void Application_BeginRequest()
+        {
+            if (Request.IsLocal)//允许本地访问启动监控
+            {
+                MiniProfiler.Start();
+            }
+        }
+
+        protected void Application_EndRequest()
+        {
+            MiniProfiler.Stop();
         }
     }
 }
